92137dff5b82fd0f96ac0f9203838aa014df2cfa
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-05-13 23:01:08 +0200

    Raised version and updated changelog

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup, Extension
 from setup_cares import cares_build_ext
 
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 setup(name             = "pycares",
       version          = __version__,
