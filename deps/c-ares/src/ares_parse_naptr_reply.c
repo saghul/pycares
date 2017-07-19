67c91dcf7d8336b10939edbe0a2e5f518a6182ca
Saúl Ibarra Corretgé <saghul@gmail.com>
2017-07-20 01:04:59 +0200

    core: update bundled c-ares to 1.13.0

--- deps/c-ares/src/ares_parse_naptr_reply.c
+++ deps/c-ares/src/ares_parse_naptr_reply.c
@@ -111,6 +111,12 @@ ares_parse_naptr_reply (const unsigned char *abuf, int alen,
           status = ARES_EBADRESP;
           break;
         }
+      /* RR must contain at least 7 bytes = 2 x int16 + 3 x name */
+      if (rr_len < 7)
+        {
+          status = ARES_EBADRESP;
+          break;
+        }
 
       /* Check if we are really looking at a NAPTR record */
       if (rr_class == C_IN && rr_type == T_NAPTR)
