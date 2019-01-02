f36e9c9df1a84f31f4196e7b32a04146a7b8e87b
Saúl Ibarra Corretgé <s@saghul.net>
2019-01-02 21:45:56 +0100

    misc: fix building wheels for unavailable Python versions

--- build-manylinux-wheels.sh
+++ build-manylinux-wheels.sh
@@ -12,8 +12,10 @@ VERSIONS="cp27-cp27mu cp33-cp33m cp34-cp34m cp35-cp35m cp36-cp36m cp37-cp37m"
 
 # Build
 for version in $VERSIONS; do
-    /opt/python/${version}/bin/python setup.py build_ext
-    /opt/python/${version}/bin/pip wheel . -w wheeltmp
+    if [[ -d /opt/python/${version} ]]; then
+        /opt/python/${version}/bin/python setup.py build_ext
+        /opt/python/${version}/bin/pip wheel . -w wheeltmp
+    fi
 done
 for whl in wheeltmp/*.whl; do
     auditwheel repair $whl -w wheelhouse
@@ -25,9 +27,11 @@ cd ..
 
 # Test (ignore if some fail, we just want to know that the module works)
 for version in $VERSIONS; do
-    /opt/python/${version}/bin/python --version
-    /opt/python/${version}/bin/pip install pycares --no-index -f /pycares/wheelhouse
-    /opt/python/${version}/bin/python -c "import pycares; print('%s - %s' % (pycares.__version__, pycares.ARES_VERSION));"
+    if [[ -d /opt/python/${version} ]]; then
+        /opt/python/${version}/bin/python --version
+        /opt/python/${version}/bin/pip install pycares --no-index -f /pycares/wheelhouse
+        /opt/python/${version}/bin/python -c "import pycares; print('%s - %s' % (pycares.__version__, pycares.ARES_VERSION));"
+    fi
 done
 
 exit 0
