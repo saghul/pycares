3731efe712ea53d28de323cdd92e71628d91b151
Saúl Ibarra Corretgé <s@saghul.net>
2019-01-08 22:58:05 +0100

    cffi: simplify
    Drop all declarations and types we don't need and make the FFI building
    file more readable.

--- pycares/build_cares.py
+++ pycares/build_cares.py
@@ -1,102 +1,48 @@
 
 import cffi
 import sys
-import os
-import re
 
-
-current_dir = os.path.dirname(__file__)
-extra_libraries = []
-
-ffi = cffi.FFI()
 
 if sys.platform == 'win32':
-    extra_libraries.extend(["ws2_32", "advapi32", "iphlpapi", "psapi"])
-    ffi.cdef(re.sub(r"\b(_In_|_Inout_|_Out_|_Outptr_|FAR)(opt_)?\b", " ",
+    PLATFORM_TYPES = """
+typedef long time_t;
+typedef long suseconds_t;
+typedef short h_addrtype_t;
+typedef short h_length_t;
+typedef short sa_family_t;
+typedef unsigned short in_port_t;
 """
-typedef unsigned short...        u_short;
-typedef unsigned long...        u_long;
-
-typedef struct fd_set {
-    ...;
-} fd_set;
-
-struct in_addr {
-    uint32_t s_addr;
-};
-
-struct in6_addr {
-    uint8_t s6_addr[16];
-};
-
-typedef struct hostent {
-    char FAR      *h_name;
-    char FAR  FAR **h_aliases;
-    short         h_addrtype;
-    short         h_length;
-    char FAR  FAR **h_addr_list;
-} HOSTENT, *PHOSTENT, FAR *LPHOSTENT;
-
-struct timeval {
-    long    tv_sec;         /* seconds */
-    long    tv_usec;        /* and microseconds */
-};
-
-struct sockaddr {
-    short  sa_family;
-    ...;
-};
-
-struct sockaddr_in {
-    short   sin_family;
-    u_short sin_port;
-    struct  in_addr sin_addr;
-    char    sin_zero[8];
-};
-
-struct sockaddr_in6 {
-    short   sin6_family;
-    u_short sin6_port;
-    u_long  sin6_flowinfo;
-    struct  in6_addr sin6_addr;
-    u_long  sin6_scope_id;
-};
-
-"""))
 else:
-    ffi.cdef("""
-/* fd_set for select and pselect.  */
-typedef struct
-{
-...;
-} fd_set;
-
-struct in_addr {
-    uint32_t s_addr;
-};
-
-struct in6_addr {
-    uint8_t s6_addr[16];
-};
-
+    PLATFORM_TYPES = """
 typedef long... time_t;
 typedef long... suseconds_t;
+typedef int h_addrtype_t;
+typedef int h_length_t;
+typedef int... sa_family_t;
+typedef uint16_t in_port_t;
+"""
+
+TYPES = """
+struct in_addr {
+    uint32_t s_addr;
+};
+
+struct in6_addr {
+    uint8_t s6_addr[16];
+};
 
 struct timeval {
-    time_t      tv_sec;     /* seconds */
-    suseconds_t tv_usec;    /* microseconds */
+    time_t      tv_sec;
+    suseconds_t tv_usec;
 };
 
 struct hostent {
-   char  *h_name;            /* official name of host */
-   char **h_aliases;         /* alias list */
-   int    h_addrtype;        /* host address type */
-   int    h_length;          /* length of address */
-   char **h_addr_list;       /* list of addresses */
+   char         *h_name;
+   char         **h_aliases;
+   h_addrtype_t h_addrtype;
+   h_length_t   h_length;
+   char         **h_addr_list;
 };
-
-typedef int... sa_family_t;
-typedef uint16_t in_port_t;
 
 struct sockaddr {
     sa_family_t sa_family;
@@ -104,24 +50,18 @@ struct sockaddr {
 };
 
 struct sockaddr_in {
-    sa_family_t       sin_family; /* Address family       */
-    in_port_t         sin_port;   /* Port number          */
-    struct in_addr    sin_addr;   /* Internet address     */
+    sa_family_t       sin_family;
+    in_port_t         sin_port;
+    struct in_addr    sin_addr;
     ...;
 };
 
 struct sockaddr_in6 {
-    sa_family_t         sin6_family;    /* AF_INET6 */
-    in_port_t           sin6_port;      /* Transport layer port # */
-    uint32_t            sin6_flowinfo;  /* IPv6 flow information */
-    struct in6_addr     sin6_addr;      /* IPv6 address */
-    uint32_t            sin6_scope_id;  /* scope id (new in RFC2553) */
+    sa_family_t         sin6_family;
+    in_port_t           sin6_port;
+    struct in6_addr     sin6_addr;
     ...;
 };
-
-""")
-
-ffi.cdef("""
 
 #define INET_ADDRSTRLEN ...
 #define INET6_ADDRSTRLEN ...
@@ -138,51 +78,36 @@ ffi.cdef("""
 #define T_SRV ...
 #define T_TXT ...
 
-
-/* ares_build.h */
 typedef int... ares_socket_t;
 typedef int... ares_socklen_t;
-#define ARES_SUCCESS            0
 
-/* Server error codes (ARES_ENODATA indicates no relevant answer) */
-#define ARES_ENODATA            1
-#define ARES_EFORMERR           2
-#define ARES_ESERVFAIL          3
-#define ARES_ENOTFOUND          4
-#define ARES_ENOTIMP            5
-#define ARES_EREFUSED           6
+#define ARES_SUCCESS            ...
 
-/* Locally generated error codes */
-#define ARES_EBADQUERY          7
-#define ARES_EBADNAME           8
-#define ARES_EBADFAMILY         9
-#define ARES_EBADRESP           10
-#define ARES_ECONNREFUSED       11
-#define ARES_ETIMEOUT           12
-#define ARES_EOF                13
-#define ARES_EFILE              14
-#define ARES_ENOMEM             15
-#define ARES_EDESTRUCTION       16
-#define ARES_EBADSTR            17
+#define ARES_ENODATA            ...
+#define ARES_EFORMERR           ...
+#define ARES_ESERVFAIL          ...
+#define ARES_ENOTFOUND          ...
+#define ARES_ENOTIMP            ...
+#define ARES_EREFUSED           ...
+#define ARES_EBADQUERY          ...
+#define ARES_EBADNAME           ...
+#define ARES_EBADFAMILY         ...
+#define ARES_EBADRESP           ...
+#define ARES_ECONNREFUSED       ...
+#define ARES_ETIMEOUT           ...
+#define ARES_EOF                ...
+#define ARES_EFILE              ...
+#define ARES_ENOMEM             ...
+#define ARES_EDESTRUCTION       ...
+#define ARES_EBADSTR            ...
+#define ARES_EBADFLAGS          ...
+#define ARES_ENONAME            ...
+#define ARES_EBADHINTS          ...
+#define ARES_ENOTINITIALIZED    ...
+#define ARES_ELOADIPHLPAPI           ...
+#define ARES_EADDRGETNETWORKPARAMS   ...
+#define ARES_ECANCELLED         ...
 
-/* ares_getnameinfo error codes */
-#define ARES_EBADFLAGS          18
-
-/* ares_getaddrinfo error codes */
-#define ARES_ENONAME            19
-#define ARES_EBADHINTS          20
-
-/* Uninitialized library error code */
-#define ARES_ENOTINITIALIZED    21          /* introduced in 1.7.0 */
-
-/* ares_library_init error codes */
-#define ARES_ELOADIPHLPAPI           22     /* introduced in 1.7.0 */
-#define ARES_EADDRGETNETWORKPARAMS   23     /* introduced in 1.7.0 */
-
-/* More error codes */
-#define ARES_ECANCELLED         24          /* introduced in 1.7.0 */
-
-/* Flag values */
 #define ARES_FLAG_USEVC         ...
 #define ARES_FLAG_PRIMARY       ...
 #define ARES_FLAG_IGNTC         ...
@@ -193,7 +118,6 @@ typedef int... ares_socklen_t;
 #define ARES_FLAG_NOCHECKRESP   ...
 #define ARES_FLAG_EDNS          ...
 
-/* Option mask values */
 #define ARES_OPT_FLAGS          ...
 #define ARES_OPT_TIMEOUT        ...
 #define ARES_OPT_TRIES          ...
@@ -211,25 +135,22 @@ typedef int... ares_socklen_t;
 #define ARES_OPT_ROTATE         ...
 #define ARES_OPT_EDNSPSZ        ...
 
-/* Nameinfo flag values */
 #define ARES_NI_NOFQDN                  ...
 #define ARES_NI_NUMERICHOST             ...
 #define ARES_NI_NAMEREQD                ...
 #define ARES_NI_NUMERICSERV             ...
 #define ARES_NI_DGRAM                   ...
-#define ARES_NI_TCP                     0
+#define ARES_NI_TCP                     ...
 #define ARES_NI_UDP                     ...
 #define ARES_NI_SCTP                    ...
 #define ARES_NI_DCCP                    ...
 #define ARES_NI_NUMERICSCOPE            ...
 #define ARES_NI_LOOKUPHOST              ...
 #define ARES_NI_LOOKUPSERVICE           ...
-/* Reserved for future use */
 #define ARES_NI_IDN                     ...
 #define ARES_NI_IDN_ALLOW_UNASSIGNED    ...
 #define ARES_NI_IDN_USE_STD3_ASCII_RULES ...
 
-/* Addrinfo flag values */
 #define ARES_AI_CANONNAME               ...
 #define ARES_AI_NUMERICHOST             ...
 #define ARES_AI_PASSIVE                 ...
@@ -237,82 +158,24 @@ typedef int... ares_socklen_t;
 #define ARES_AI_V4MAPPED                ...
 #define ARES_AI_ALL                     ...
 #define ARES_AI_ADDRCONFIG              ...
-/* Reserved for future use */
 #define ARES_AI_IDN                     ...
 #define ARES_AI_IDN_ALLOW_UNASSIGNED    ...
 #define ARES_AI_IDN_USE_STD3_ASCII_RULES ...
 #define ARES_AI_CANONIDN                ...
-
 #define ARES_AI_MASK ...
-#define ARES_GETSOCK_MAXNUM ... /* ares_getsock() can return info about this
-                                  many sockets */
+
+#define ARES_GETSOCK_MAXNUM ...
 int ARES_GETSOCK_READABLE(int, int);
 int ARES_GETSOCK_WRITABLE(int, int);
 
-/* c-ares library initialization flag values */
-#define ARES_LIB_INIT_NONE   ...
-#define ARES_LIB_INIT_WIN32  ...
 #define ARES_LIB_INIT_ALL    ...
 
-
-/*
- * Typedef our socket type
- */
-//typedef int ares_socket_t;
 #define ARES_SOCKET_BAD ...
-
 
 typedef void (*ares_sock_state_cb)(void *data,
                                    ares_socket_t socket_fd,
                                    int readable,
                                    int writable);
-
-struct apattern;
-
-/* NOTE about the ares_options struct to users and developers.
-
-   This struct will remain looking like this. It will not be extended nor
-   shrunk in future releases, but all new options will be set by ares_set_*()
-   options instead of with the ares_init_options() function.
-
-   Eventually (in a galaxy far far away), all options will be settable by
-   ares_set_*() options and the ares_init_options() function will become
-   deprecated.
-
-   When new options are added to c-ares, they are not added to this
-   struct. And they are not "saved" with the ares_save_options() function but
-   instead we encourage the use of the ares_dup() function. Needless to say,
-   if you add config options to c-ares you need to make sure ares_dup()
-   duplicates this new option.
-
- */
-struct ares_options {
-  int flags;
-  int timeout; /* in seconds or milliseconds, depending on options */
-  int tries;
-  int ndots;
-  unsigned short udp_port;
-  unsigned short tcp_port;
-  int socket_send_buffer_size;
-  int socket_receive_buffer_size;
-  struct in_addr *servers;
-  int nservers;
-  char **domains;
-  int ndomains;
-  char *lookups;
-  ares_sock_state_cb sock_state_cb;
-  void *sock_state_cb_data;
-  struct apattern *sortlist;
-  int nsort;
-  int ednspsz;
-};
-
-struct hostent;
-struct timeval;
-struct sockaddr;
-struct ares_channeldata;
-
-typedef struct ares_channeldata *ares_channel;
 
 typedef void (*ares_callback)(void *arg,
                               int status,
@@ -335,6 +198,112 @@ typedef int  (*ares_sock_create_callback)(ares_socket_t socket_fd,
                                           int type,
                                           void *data);
 
+struct apattern;
+
+struct ares_channeldata;
+typedef struct ares_channeldata *ares_channel;
+
+struct ares_options {
+  int flags;
+  int timeout;
+  int tries;
+  int ndots;
+  unsigned short udp_port;
+  unsigned short tcp_port;
+  int socket_send_buffer_size;
+  int socket_receive_buffer_size;
+  struct in_addr *servers;
+  int nservers;
+  char **domains;
+  int ndomains;
+  char *lookups;
+  ares_sock_state_cb sock_state_cb;
+  void *sock_state_cb_data;
+  struct apattern *sortlist;
+  int nsort;
+  int ednspsz;
+};
+
+struct ares_in6_addr {
+  union {
+    unsigned char _S6_u8[16];
+  } _S6_un;
+};
+
+struct ares_addrttl {
+  struct in_addr ipaddr;
+  int            ttl;
+};
+
+struct ares_addr6ttl {
+  struct ares_in6_addr ip6addr;
+  int             ttl;
+};
+
+struct ares_srv_reply {
+  struct ares_srv_reply  *next;
+  char                   *host;
+  unsigned short          priority;
+  unsigned short          weight;
+  unsigned short          port;
+  int                     ttl;
+};
+
+struct ares_mx_reply {
+  struct ares_mx_reply   *next;
+  char                   *host;
+  unsigned short          priority;
+  int                     ttl;
+};
+
+struct ares_txt_reply {
+  struct ares_txt_reply  *next;
+  unsigned char          *txt;
+  size_t                  length;
+  int                     ttl;
+};
+
+struct ares_txt_ext {
+  struct ares_txt_ext      *next;
+  unsigned char            *txt;
+  size_t                   length;
+  unsigned char            record_start;
+  int                      ttl;
+};
+
+struct ares_naptr_reply {
+  struct ares_naptr_reply *next;
+  unsigned char           *flags;
+  unsigned char           *service;
+  unsigned char           *regexp;
+  char                    *replacement;
+  unsigned short           order;
+  unsigned short           preference;
+  int                      ttl;
+};
+
+struct ares_soa_reply {
+  char        *nsname;
+  char        *hostmaster;
+  unsigned int serial;
+  unsigned int refresh;
+  unsigned int retry;
+  unsigned int expire;
+  unsigned int minttl;
+  int          ttl;
+};
+
+struct ares_addr_node {
+  struct ares_addr_node *next;
+  int family;
+  union {
+    struct in_addr       addr4;
+    struct ares_in6_addr addr6;
+  } addr;
+};
+"""
+
+FUNCTIONS = """
 int ares_library_init(int flags);
 
 void ares_library_cleanup(void);
@@ -360,17 +329,11 @@ void ares_destroy(ares_channel channel);
 
 void ares_cancel(ares_channel channel);
 
-/* These next 3 configure local binding for the out-going socket
- * connection.  Use these to specify source IP and/or network device
- * on multi-homed systems.
- */
 void ares_set_local_ip4(ares_channel channel, unsigned int local_ip);
 
-/* local_ip6 should be 16 bytes in length */
 void ares_set_local_ip6(ares_channel channel,
                                      const unsigned char* local_ip6);
 
-/* local_dev_name should be null terminated. */
 void ares_set_local_dev(ares_channel channel,
                                      const char* local_dev_name);
 
@@ -423,10 +386,6 @@ void ares_getnameinfo(ares_channel channel,
                                    ares_nameinfo_callback callback,
                                    void *arg);
 
-int ares_fds(ares_channel channel,
-                          fd_set *read_fds,
-                          fd_set *write_fds);
-
 int ares_getsock(ares_channel channel,
                               ares_socket_t *socks,
                               int numsocks);
@@ -434,10 +393,6 @@ int ares_getsock(ares_channel channel,
 struct timeval *ares_timeout(ares_channel channel,
                                           struct timeval *maxtv,
                                           struct timeval *tv);
-
-void ares_process(ares_channel channel,
-                               fd_set *read_fds,
-                               fd_set *write_fds);
 
 void ares_process_fd(ares_channel channel,
                                   ares_socket_t read_fd,
@@ -471,93 +426,6 @@ int ares_expand_string(const unsigned char *encoded,
                                     int alen,
                                     unsigned char **s,
                                     long *enclen);
-
-/*
- * NOTE: before c-ares 1.7.0 we would most often use the system in6_addr
- * struct below when ares itself was built, but many apps would use this
- * private version since the header checked a HAVE_* define for it. Starting
- * with 1.7.0 we always declare and use our own to stop relying on the
- * system's one.
- */
-struct ares_in6_addr {
-  union {
-    unsigned char _S6_u8[16];
-  } _S6_un;
-};
-
-struct ares_addrttl {
-  struct in_addr ipaddr;
-  int            ttl;
-};
-
-struct ares_addr6ttl {
-  struct ares_in6_addr ip6addr;
-  int             ttl;
-};
-
-struct ares_srv_reply {
-  struct ares_srv_reply  *next;
-  char                   *host;
-  unsigned short          priority;
-  unsigned short          weight;
-  unsigned short          port;
-  int                     ttl;
-};
-
-struct ares_mx_reply {
-  struct ares_mx_reply   *next;
-  char                   *host;
-  unsigned short          priority;
-  int                     ttl;
-};
-
-struct ares_txt_reply {
-  struct ares_txt_reply  *next;
-  unsigned char          *txt;
-  size_t                  length;  /* length excludes null termination */
-  int                     ttl;
-};
-
-/* NOTE: This structure is a superset of ares_txt_reply */
-struct ares_txt_ext {
-  struct ares_txt_ext      *next;
-  unsigned char            *txt;
-  size_t                   length;
-  /* 1 - if start of new record
-   * 0 - if a chunk in the same record */
-  unsigned char            record_start;
-  int                     ttl;
-};
-
-struct ares_naptr_reply {
-  struct ares_naptr_reply *next;
-  unsigned char           *flags;
-  unsigned char           *service;
-  unsigned char           *regexp;
-  char                    *replacement;
-  unsigned short           order;
-  unsigned short           preference;
-  int                      ttl;
-};
-
-struct ares_soa_reply {
-  char        *nsname;
-  char        *hostmaster;
-  unsigned int serial;
-  unsigned int refresh;
-  unsigned int retry;
-  unsigned int expire;
-  unsigned int minttl;
-  int          ttl;
-};
-
-/*
-** Parse the buffer, starting at *abuf and of length alen bytes, previously
-** obtained from an ares_search call.  Put the results in *host, if nonnull.
-** Also, if addrttls is nonnull, put up to *naddrttls IPv4 addresses along with
-** their TTLs in that array, and set *naddrttls to the number of addresses
-** so written.
-*/
 
 int ares_parse_a_reply(const unsigned char *abuf,
                                     int alen,
@@ -611,22 +479,8 @@ void ares_free_data(void *dataptr);
 
 const char *ares_strerror(int code);
 
-/* TODO:  Hold port here as well. */
-struct ares_addr_node {
-  struct ares_addr_node *next;
-  int family;
-  union {
-    struct in_addr       addr4;
-    struct ares_in6_addr addr6;
-  } addr;
-};
-
 int ares_set_servers(ares_channel channel,
                                   struct ares_addr_node *servers);
-
-/* Incomming string format: host[:port][,host[:port]]... */
-int ares_set_servers_csv(ares_channel channel,
-                                      const char* servers);
 
 int ares_get_servers(ares_channel channel,
                                   struct ares_addr_node **servers);
@@ -636,9 +490,9 @@ const char *ares_inet_ntop(int af, const void *src, char *dst,
 
 int ares_inet_pton(int af, const char *src, void *dst);
 
-""")
+"""
 
-ffi.set_source("_pycares_cffi", """
+INCLUDES = """
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 # include <WinSock2.h>
@@ -648,13 +502,13 @@ ffi.set_source("_pycares_cffi", """
 # include <netdb.h> /* struct hostent */
 # include <netinet/in.h> /* struct sockaddr_in/sockaddr_in6 */
 #endif
+#include <nameser.h>
 #define CARES_STATICLIB 1 /* static link it */
 #include <ares.h>
-# include <nameser.h>
-
-""", libraries=(extra_libraries), include_dirs=[os.path.join(current_dir, "../../deps/c-ares/src"),], library_dirs=[os.path.join(current_dir, "../../deps/c-ares"),])
+"""
 
 
-if __name__ == "__main__":
-    ffi.compile(verbose=True)
+ffi = cffi.FFI()
+ffi.cdef(PLATFORM_TYPES + TYPES + FUNCTIONS)
+ffi.set_source("_pycares_cffi", INCLUDES)
 
