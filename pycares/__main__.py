06e0632d5a30e1002df64c0d0b0353e2f3efbb87
Saúl Ibarra Corretgé <s@saghul.net>
2019-01-16 11:28:47 +0100

    core: rewrite builtin dig-like output
    python -m pycares example.com now gives an output similar to dig.

--- pycares/__main__.py
+++ pycares/__main__.py
@@ -1,4 +1,5 @@
 
+import collections.abc
 import pycares
 import select
 import socket
@@ -25,7 +26,38 @@ def cb(result, error):
     if error is not None:
         print('Error: (%d) %s' % (error, pycares.errno.strerror(error)))
     else:
-        print(result)
+        parts = [
+            ';; QUESTION SECTION:',
+            ';%s\t\t\tIN\t%s' % (hostname, qtype.upper()),
+            '',
+            ';; ANSWER SECTION:'
+        ]
+
+        if not isinstance(result, collections.abc.Iterable):
+            result = [result]
+
+        for r in result:
+            txt = '%s\t\t%d\tIN\t%s' % (hostname, r.ttl, qtype.upper())
+            if query_type == pycares.QUERY_TYPE_A or query_type == pycares.QUERY_TYPE_AAAA:
+                parts.append('%s\t%s' % (txt, r.host))
+            elif query_type == pycares.QUERY_TYPE_CNAME:
+                parts.append('%s\t%s' % (txt, r.cname))
+            elif query_type == pycares.QUERY_TYPE_MX:
+                parts.append('%s\t%d %s' % (txt, r.priority, r.host))
+            elif query_type == pycares.QUERY_TYPE_NAPTR:
+                parts.append('%s\t%d %d "%s" "%s" "%s" %s' % (txt, r.order, r.preference, r.flags, r.service, r.regex, r.replacement))
+            elif query_type == pycares.QUERY_TYPE_NS:
+                parts.append('%s\t%s' % (txt, r.host))
+            elif query_type == pycares.QUERY_TYPE_PTR:
+                parts.append('%s\t%s' % (txt, r.name))
+            elif query_type == pycares.QUERY_TYPE_SOA:
+                parts.append('%s\t%s %s %d %d %d %d %d' % (txt, r.nsname, r.hostmaster, r.serial, r.refresh, r.retry, r.expires, r.minttl))
+            elif query_type == pycares.QUERY_TYPE_SRV:
+                parts.append('%s\t%d %d %d %s' % (txt, r.priority, r.weight, r.port, r.host))
+            elif query_type == pycares.QUERY_TYPE_TXT:
+                parts.append('%s\t"%s"' % (txt, r.text))
+
+        print('\n'.join(parts))
 
 
 channel = pycares.Channel()
