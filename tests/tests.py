9c2a6d4334d0dd2da7bdee90706343839ff6d2f4
Hassan Abouelela <hassan@hassanamr.com>
2021-05-06 11:00:09 +0300

    Fixes Chunked TXT Test
    The website used for the old test changed it's records, and invalidated
    the test.

--- tests/tests.py
+++ tests/tests.py
@@ -226,13 +226,20 @@ class DNSTest(unittest.TestCase):
         self.result, self.errorno = None, None
         def cb(result, errorno):
             self.result, self.errorno = result, errorno
-        self.channel.query('s-pulse.co.jp', pycares.QUERY_TYPE_TXT, cb)
+        self.channel.query('google.com', pycares.QUERY_TYPE_TXT, cb)
         self.wait()
         self.assertNoError(self.errorno)
-        # s-pulse.co.jp.    3600	IN	TXT	"MS=ms18955624"
-        # s-pulse.co.jp.    3600	IN	TXT	"amazonses:lOgEcA9DwKFkIusIbgjpvZ2kCxaVADMlaxq9hSO3k4o="
-        # s-pulse.co.jp.    3600	IN	TXT	"v=spf1 " "include:spf-bma.mpme.jp ip4:202.248.11.9 ip4:202.248.11.10 " "ip4:218.223.68.132 ip4:218.223.68.77 ip4:210.254.139.121 " "ip4:211.128.73.121 ip4:210.254.139.122 ip4:211.128.73.122 " "ip4:210.254.139.123 ip4:211.128.73.123 ip4:210.254.139.124 " "ip4:211.128.73.124 ip4:210.254.139.13 ip4:211.128.73.13 " "ip4:52.68.199.198 include:spf.betrend.com " "include:spf.protection.outlook.com include:crmstyle.com " "~all"
-        self.assertEqual(len(self.result), 3)
+        # > dig -t txt google.com
+        # google.com.             3593    IN      TXT     "docusign=05958488-4752-4ef2-95eb-aa7ba8a3bd0e"
+        # google.com.             3593    IN      TXT     "globalsign-smime-dv=CDYX+XFHUw2wml6/Gb8+59BsH31KzUr6c1l2BPvqKX8="
+        # google.com.             3593    IN      TXT     "facebook-domain-verification=22rm551cu4k0ab0bxsw536tlds4h95"
+        # google.com.             3593    IN      TXT     "docusign=1b0a6754-49b1-4db5-8540-d2c12664b289"
+        # google.com.             3593    IN      TXT     "MS=E4A68B9AB2BB9670BCE15412F62916164C0B20BB"
+        # google.com.             3593    IN      TXT     "v=spf1 include:_spf.google.com ~all"
+        # google.com.             3593    IN      TXT     "google-site-verification=wD8N7i1JTNTkezJ49swvWW48f8_9xveREV4oB-0Hf5o"
+        # google.com.             3593    IN      TXT     "apple-domain-verification=30afIBcvSuDV2PLX"
+        # google.com.             3593    IN      TXT     "google-site-verification=TV9-DBe4R80X4v0M4U_bd_J9cpOJM0nikft0jAgjmsQ"
+        self.assertEqual(len(self.result), 9)
 
     def test_query_txt_bytes1(self):
         self.result, self.errorno = None, None
