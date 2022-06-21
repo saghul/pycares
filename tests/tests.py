8026e66ce7188f30020bd9a63fda020df4122117
aviadhahami <aviadhahami@gmail.com>
2022-06-21 14:07:51 +0300

    tests: updated google TXT records for an outdated test

--- tests/tests.py
+++ tests/tests.py
@@ -300,16 +300,18 @@ class DNSTest(unittest.TestCase):
         self.wait()
         self.assertNoError(self.errorno)
         # > dig -t txt google.com
-        # google.com.             3593    IN      TXT     "docusign=05958488-4752-4ef2-95eb-aa7ba8a3bd0e"
-        # google.com.             3593    IN      TXT     "globalsign-smime-dv=CDYX+XFHUw2wml6/Gb8+59BsH31KzUr6c1l2BPvqKX8="
-        # google.com.             3593    IN      TXT     "facebook-domain-verification=22rm551cu4k0ab0bxsw536tlds4h95"
-        # google.com.             3593    IN      TXT     "docusign=1b0a6754-49b1-4db5-8540-d2c12664b289"
-        # google.com.             3593    IN      TXT     "MS=E4A68B9AB2BB9670BCE15412F62916164C0B20BB"
-        # google.com.             3593    IN      TXT     "v=spf1 include:_spf.google.com ~all"
-        # google.com.             3593    IN      TXT     "google-site-verification=wD8N7i1JTNTkezJ49swvWW48f8_9xveREV4oB-0Hf5o"
-        # google.com.             3593    IN      TXT     "apple-domain-verification=30afIBcvSuDV2PLX"
-        # google.com.             3593    IN      TXT     "google-site-verification=TV9-DBe4R80X4v0M4U_bd_J9cpOJM0nikft0jAgjmsQ"
-        self.assertEqual(len(self.result), 9)
+        # google.com.		3270	IN	TXT	"google-site-verification=TV9-DBe4R80X4v0M4U_bd_J9cpOJM0nikft0jAgjmsQ"
+        # google.com.		3270	IN	TXT	"atlassian-domain-verification=5YjTmWmjI92ewqkx2oXmBaD60Td9zWon9r6eakvHX6B77zzkFQto8PQ9QsKnbf4I"
+        # google.com.		3270	IN	TXT	"docusign=05958488-4752-4ef2-95eb-aa7ba8a3bd0e"
+        # google.com.		3270	IN	TXT	"facebook-domain-verification=22rm551cu4k0ab0bxsw536tlds4h95"
+        # google.com.		3270	IN	TXT	"google-site-verification=wD8N7i1JTNTkezJ49swvWW48f8_9xveREV4oB-0Hf5o"
+        # google.com.		3270	IN	TXT	"apple-domain-verification=30afIBcvSuDV2PLX"
+        # google.com.		3270	IN	TXT	"webexdomainverification.8YX6G=6e6922db-e3e6-4a36-904e-a805c28087fa"
+        # google.com.		3270	IN	TXT	"MS=E4A68B9AB2BB9670BCE15412F62916164C0B20BB"
+        # google.com.		3270	IN	TXT	"v=spf1 include:_spf.google.com ~all"
+        # google.com.		3270	IN	TXT	"globalsign-smime-dv=CDYX+XFHUw2wml6/Gb8+59BsH31KzUr6c1l2BPvqKX8="
+        # google.com.		3270	IN	TXT	"docusign=1b0a6754-49b1-4db5-8540-d2c12664b289"
+        self.assertEqual(len(self.result), 11)
 
     def test_query_txt_bytes1(self):
         self.result, self.errorno = None, None
