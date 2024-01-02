c9b17d4a2a02a63b563084bce99aa041cbeabb1d
Scott Kitterman <scott@kitterman.com>
2024-01-02 12:45:01 -0500

    Update test_idna_encoding_query_a with new errno to align to new c-ares version

--- tests/test_all.py
+++ tests/test_all.py
@@ -569,7 +569,7 @@ class DNSTest(unittest.TestCase):
         # try encoding it as utf-8
         self.channel.query(host.encode(), pycares.QUERY_TYPE_A, cb)
         self.wait()
-        self.assertEqual(self.errorno, pycares.errno.ARES_ENOTFOUND)
+        self.assertEqual(self.errorno, pycares.errno.ARES_EBADNAME)
         self.assertEqual(self.result, None)
         # use it as is (it's IDNA encoded internally)
         self.channel.query(host, pycares.QUERY_TYPE_A, cb)
