b50b86e6109ab72768164d65495e1ed26bf0ea19
Saúl Ibarra Corretgé <s@saghul.net>
2025-07-01 14:18:46 +0200

    Fix OpenBSD build

--- deps/build-config/config_openbsd/ares_config.h
+++ deps/build-config/config_openbsd/ares_config.h
@@ -183,7 +183,7 @@
 #define HAVE_LONGLONG 1
 
 /* Define to 1 if you have the malloc.h header file. */
-#define HAVE_MALLOC_H 1
+/* #define HAVE_MALLOC_H 1 */
 
 /* Define to 1 if you have the memory.h header file. */
 #define HAVE_MEMORY_H 1
