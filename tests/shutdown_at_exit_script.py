9998e1e66e6cebd2b1ef248ad35fe245bb348987
Vizonex <VizonexBusiness@gmail.com>
2025-07-18 11:36:41 -0500

    reformat tests

--- tests/shutdown_at_exit_script.py
+++ tests/shutdown_at_exit_script.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 """Script to test that shutdown thread handles interpreter shutdown gracefully."""
 
-import pycares
 import sys
+
+import pycares
 
 # Create a channel
 channel = pycares.Channel()
+
 
 # Start a query to ensure pending handles
 def callback(result, error):
     pass
 
-channel.query('example.com', pycares.QUERY_TYPE_A, callback)
+
+channel.query("example.com", pycares.QUERY_TYPE_A, callback)
 
 # Exit immediately - the channel will be garbage collected during interpreter shutdown
 # This should not raise PythonFinalizationError
